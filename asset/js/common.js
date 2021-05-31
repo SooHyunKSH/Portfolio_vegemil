cb4a680b31f167385edbc8ce4c2c6cccd058cf95
SooHyunKSH <hyun29075691@gmail.com>
2021-05-31 16:32:28 +0900

    add subpage

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,3 +1,28 @@
 $(function(){
-  //script 입력영역
+    /* click list li 클릭 시 display0$ 나오기 */
+    $(".click_list li").click(function() {
+      $(".click_list li").removeClass("on");
+      $(this).addClass("on");
+      let indexNum = $(this).index() + 1;
+      $(".display_wrap .display_box").css("display", "none");
+      $(".display_wrap .display_box").each(function() {
+        let thisIndex = $(this).index() + 1;
+        if (indexNum == thisIndex) {
+          $(this).css("display", "block");
+        }
+        console.log($(this).attr("class"))
+        console.log($(this).index() + 1)
+      });
+    });
+    $(".close_btn").click(function() {
+      $(".display_wrap .display_box").css("display", "none");
+    });
+
+    /* suport 아코디언 FAQ */
+    $("dl dt").on("click",function(){
+      if( $("+dd",this).css("display") == "none"){
+        $("dl dd").slideUp();
+        $("+dd",this).slideDown();
+      }
+    });
 });
